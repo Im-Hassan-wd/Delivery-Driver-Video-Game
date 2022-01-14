99db1d29728a09d5399de3865f466fce5757d30d
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-14 19:57:05 +0100

    Mking the hard coded codes dynamic

--- Driver.cs
+++ Driver.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Driver : MonoBehaviour {
-
+public class Driver : MonoBehaviour 
+{
+    // defining speed,
+    float steerSpeed = 0.1f;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.Rotate(0, 0, 0.1f);
+	void Update () 
+    {
+		transform.Rotate(0, 0, steerSpeed);
         transform.Translate(0.1f, 5, 0);
 	}
 }
