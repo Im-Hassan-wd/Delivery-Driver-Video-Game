b2b294a8501d7e456007f9d415df0ce06b882540
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-17 11:48:39 +0100

    picking and delivering page

--- Delivery.cs
+++ Delivery.cs
@@ -15,11 +15,13 @@ public class Delivery : MonoBehaviour
 		if (other.tag == "Package") 
 		{
 			Debug.Log ("Package picked up");
+			hasPackage = true;
 		}
 		// if it's Customer
-		if (other.tag == "Customer") 
+		if (other.tag == "Customer" && hasPackage) 
 		{
 			Debug.Log ("Package delivered");
+			hasPackage = false;
 		}
 
 	}
