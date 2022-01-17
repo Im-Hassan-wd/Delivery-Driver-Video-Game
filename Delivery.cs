2db9ebeb4564d96238be1c43fc8542b890521349
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-17 12:21:31 +0100

    picking and delivering packages

--- Delivery.cs
+++ Delivery.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour 
 {
-	// Colliders and triggers
-	void OnCollisionEnter2D(Collision2D other) {
+	// change car color
+	[SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+	[SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+	//re moved picked page
+	[SerializeField] float destroyDelay = 0.5f;
+	bool hasPackage;
+
+	//store refrence for sprite renderer
+	SpriteRenderer spriteRenderer;
+
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void OnCollisionEnter2D(Collision2D other) 
+	{
 		Debug.Log("Ouch!");
 	}
-	
+
 	void OnTriggerEnter2D(Collider2D other) 
 	{
 		// if it's packagee
@@ -16,6 +32,7 @@ public class Delivery : MonoBehaviour
 		{
 			Debug.Log ("Package picked up");
 			hasPackage = true;
+			spriteRenderer.color = hasPackageColor;
 			Destroy (other.gameObject, destroyDelay);
 		}
 		// if it's Customer
@@ -23,6 +40,7 @@ public class Delivery : MonoBehaviour
 		{
 			Debug.Log ("Package delivered");
 			hasPackage = false;
+			spriteRenderer.color = noPackageColor;
 		}
 
 	}
