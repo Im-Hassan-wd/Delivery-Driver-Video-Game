425f231808db01ccb590ef6e0a869b7fead760f9
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-17 11:57:31 +0100

    picking and delivering page

--- Delivery.cs
+++ Delivery.cs
@@ -12,10 +12,11 @@ public class Delivery : MonoBehaviour
 	void OnTriggerEnter2D(Collider2D other) 
 	{
 		// if it's packagee
-		if (other.tag == "Package") 
+		if (other.tag == "Package" && !hasPackage) 
 		{
 			Debug.Log ("Package picked up");
 			hasPackage = true;
+			Destroy (other.gameObject, destroyDelay);
 		}
 		// if it's Customer
 		if (other.tag == "Customer" && hasPackage) 
